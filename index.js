e2ebbebf369d858d3ec05c46cdc089f626687099
Diamond Toliver <diamondtoliver@Diamonds-MacBook-Pro.local>
2021-12-16 13:57:35 -0500

    added rollbar

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require(`express`)
 const path = require('path')
+
+
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '9d48bb3223284fc682fa31bc0f240134',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 const app = express()
+
+
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file serverd successfully.')
 })
+
+// include and initialize the rollbar library with your access token
+
 
 const port = process.env.PORT || 4545
 app.listen(port, () => console.log(` It works!${port}`))
